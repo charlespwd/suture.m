3ed3b54011df4395fbbe1765b8c46adad368c97f
Charles-P. Clermont <charles.pclermont@gmail.com>
2014-11-04 21:31:22 -0500

    Updated the sut function

--- sut.m
+++ sut.m
@@ -1,21 +1,21 @@
-function z = sut(N, Ph, Pphi, dt)
+function z = sut(N, Pphi, Ph, dt)
   % SUT  plot suture with given N, P(h) and P(phi)
   map = @(x, f) arrayfun(f, x);
+
+  if nargin < 2
+    Pphi = 0.10;
+  end
+
+  if nargin < 3
+    Ph = 0.085;
+  end
 
   if nargin < 4
     dt = 0.05;
   end
 
-  if nargin < 3
-    Pphi = 0.005;
-  end
-
-  if nargin < 2
-    Ph = 0.015;
-  end
-
-  Ahs   = abs(rand_sphere_coord(sqrt(Ph), N));
   Aphis = abs(rand_sphere_coord(sqrt(Pphi), N));
-  z = map(0:dt:(2*pi+dt), @(t) suture(Ahs, Aphis, t));
+  Ahs   = abs(rand_sphere_coord(sqrt(Ph), N));
+  z = map(0:dt:(2*pi+dt), @(t) suture(Aphis, Ahs, t));
   plot(z);
 end
