1f14f3fbdc3187940d6d27c20083765889d42f59
Charles-P. Clermont <charles.pclermont@gmail.com>
2014-11-17 14:08:03 -0500

    refactored plot_fcpcurves.

--- plot_fcpcurves.m
+++ plot_fcpcurves.m
@@ -1,25 +1,32 @@
-function plot_fcpcurves(As)
+function fig_id = plot_fcpcurves(As, fontsize)
   import suture.fcmemo;
   if nargin < 1; As = {-1:.5:1 1:-1:-1}; end;
+  if nargin < 2; fontsize = '14'; end;
   if ~is_descending(As{2}); As{2} = sort(As{2}, 'descend'); end;
   memo = 0; fig_id = figure;
 
   for i = 1:length(As{1})
     for j = 1:length(As{2})
-      memo = subplotcurve(@fcmemo, As, i, j, memo);
+      memo = subplotcurve(@fcmemo, As, i, j, memo, fontsize);
       disp_percent_progress(i,j,length(As{1}),length(As{2}));
     end
   end
 
+  width = 1200;
+  height = 1000;
+  set(fig_id, 'Position', [0 0 width height])
+  disp(figure_filename(As));
+  saveas(fig_id, figure_filename(As), 'png')
+  pause(2)
   saveas(fig_id, figure_filename(As), 'png')
 end
 
-function memo = subplotcurve(memofn, As, i, j, memo)
+function memo = subplotcurve(memofn, As, i, j, memo, fontsize)
   [A1, A2] = destructure(As);
-  A = [A1(i), A2(j)];
+  A = [A1(i), 0, A2(j)];
   [z, memo] = memofn(A, false, memo);
   subplot(length(A2), length(A1), subplot_index(i, j, length(A1)));
-  prettyplot(z, figure_title(A));
+  prettyplot(z, figure_title(A, fontsize));
 end
 
 function prettyplot(z, plot_title)
@@ -52,19 +59,28 @@ function r = range_plus_padding(z, percentPad)
   r = [R(1) - padding, R(2) + padding];
 end
 
+function m = min_max_dt(A)
+  m = [min(A) range(A)/(length(A)-1) max(A)];
+end
+
 function is_desc = is_descending(A)
    is_desc = length(A) > 2 && A(2) < A(1);
 end
 
-function t = figure_title(A)
-  t = strcat('[', num2str(A, 2) , ']');
+function t = figure_title(A, fontsize)
+  t = strcat('\fontsize{', fontsize, '}[', num2str(A, 1) , ']');
   % disp(t);
 end
 
 function fn = figure_filename(As)
-  tostr    = @(x) strrep(num2str(x), '.', ',');
-  filename = @(A) strcat('(', tostr(range_plus_padding(A{1},0)), '),(', tostr(range_plus_padding(A{2},0)) ,')');
-  path     = @(A) strcat('figs/', filename(A));
+  compose4    = @(a, b, c, d, e) a(b(c(d(e))));
+  colonjoin   = @(x) strjoin(x, '_');
+  stringify   = @(a) arrayfun(@num2str, a, 'UniformOutput', false);
+  stripspaces = @(x) strrep(x, ' ', '');
+  range2str   = @(x) compose4(stripspaces, colonjoin, @cell, stringify, x);
+  tostr       = @(x) strrep(range2str(x), '.', ',');
+  filename    = @(A) strcat('(', tostr(min_max_dt(A{1})), '),(', tostr(min_max_dt(A{2})) ,')');
+  path        = @(A) strcat('figs/', filename(A));
   fn = path(As);
 end
 
